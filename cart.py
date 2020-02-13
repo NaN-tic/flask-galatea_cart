228ee50392da382d6a3547eb64ba96c5ace098d9
Raimon Esteve <raimonesteve@gmail.com>
2020-02-13 11:28:42 +0100

    Duplicated countries to assign in Shipment/Invoice Address Form

--- cart.py
+++ cart.py
@@ -1013,6 +1013,7 @@ def cart_list(lang):
     website, = websites
 
     shop = Shop(SHOP)
+    countries = [(c.id, c.name) for c in shop.esale_countrys]
 
     # Products and lines
     domain = [
@@ -1121,13 +1122,11 @@ def cart_list(lang):
     # Invoice address country options
     form_invoice_address = InvoiceAddressForm(
         country=shop.esale_country.id)
-    countries = [(c.id, c.name) for c in shop.esale_countrys]
     form_invoice_address.invoice_country.choices = countries
 
     # Shipment address country options
     form_shipment_address = ShipmentAddressForm(
         country=shop.esale_country.id)
-    countries = [(c.id, c.name) for c in shop.esale_countrys]
     form_shipment_address.shipment_country.choices = countries
 
     # Payment types options
